33f67f089e3d1cfb3e8ed92b6394184c6b7e0a11
UsfHajaj <yosefys236@gmail.com>
2025-04-27 16:24:22 +0300

    New Update

---
+++ Repositories/Implementions/EnrollmentsRepository.cs
@@ -0,0 +1,54 @@
+﻿using LMS.Models.Context;
+using LMS.Models.Interaction;
+using LMS.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Repositories.Implementions
+{
+    public class EnrollmentsRepository:GenericRepository<Enrollment>, IEnrollmentsRepository
+    {
+        public EnrollmentsRepository(ApplicationDbContext context):base(context)
+        {
+        }
+
+        public async Task<Enrollment> EnrollmentByStudentIdAndCourseId(string studentId, int courseId)
+        {
+            var enrollment =await _dbSet
+                .Where(e=>e.StudentId == studentId && e.CourseId == courseId)
+                .FirstOrDefaultAsync();
+            return enrollment;
+        }
+
+        public async Task<IEnumerable<Enrollment>> EnrollmentsByCourseId(int courseId)
+        {
+            var enrollments = await _dbSet
+                .Where(m=>m.CourseId == courseId)
+                .ToListAsync();
+            return enrollments;
+        }
+
+        public async Task<IEnumerable<Enrollment>> EnrollmentsByStudentId(string studentId)
+        {
+            var enrollments = await _dbSet
+                .Where(m => m.StudentId == studentId)
+                .ToListAsync();
+            return enrollments;
+        }
+
+        public async Task<bool> IsCompleted(string studentId, int courseId)
+        {
+            return await _dbSet
+                .AnyAsync(e => e.StudentId == studentId 
+                && e.CourseId == courseId 
+                && e.IsCompleted);
+        }
+
+        public async Task<bool> IsEnrolled(string studentId, int courseId)
+        {
+            return await _dbSet
+                .AnyAsync(e => e.StudentId == studentId
+                &&e.CourseId==e.CourseId);
+        }
+    }
+}
---
+++ Repositories/Interfaces/IEnrollmentsRepository.cs
@@ -0,0 +1,14 @@
+﻿using LMS.Models.Interaction;
+
+namespace LMS.Repositories.Interfaces
+{
+    public interface IEnrollmentsRepository:IGenericRepository<Enrollment>
+    {
+        Task<Enrollment> EnrollmentByStudentIdAndCourseId(string studentId, int courseId);
+        Task<IEnumerable<Enrollment>> EnrollmentsByStudentId(string studentId);
+        Task<IEnumerable<Enrollment>> EnrollmentsByCourseId(int courseId);
+        Task<bool> IsEnrolled(string studentId, int courseId);
+        Task<bool> IsCompleted(string studentId, int courseId);
+
+    }
+}
